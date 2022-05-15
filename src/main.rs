4542aa486dce3812ea056e8720b67f0552a7b4c0
Supachai Chaimangua (Torrotto) <tortechnocom@gmail.com>
2022-05-15 13:47:05 +0700

    guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-fn main() {
-    println!("Rust learning");
-    // mutable
-    println!("----------");
-    println!(">> mutable / immutable");
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-    // immutable
-    println!(">> immutable");
-    let y = 5;
-    println!("The value of y is: {}", x);
-    // y = 6;
-    println!("The value of y is: {}", x);
-    println!("----------");
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("Constants: {}", THREE_HOURS_IN_SECONDS);
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    // println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    
 }
