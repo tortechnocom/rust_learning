b5f2d2ff5bda34d3a22e83dc2e22f9ca298db733
Supachai Chaimangua (Torrotto) <tortechnocom@gmail.com>
2022-05-15 13:48:38 +0700

    hello rust

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
 fn main() {
-    println!("Rust learning");
-    // mutable
-    println!("----------");
-    println!(">> mutable / immutable");
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-    // immutable
-    println!(">> immutable");
-    let y = 5;
-    println!("The value of y is: {}", x);
-    // y = 6;
-    println!("The value of y is: {}", x);
-    println!("----------");
-
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("Constants: {}", THREE_HOURS_IN_SECONDS);
+    println!("Hello, world!");
 }
